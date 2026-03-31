c02d357f6b19010b44632e12aac839bbaf79a13d
ashu-choudhury <ashu@ashuchoudhury.in>
2026-03-31 09:10:59 +0530

    Fix JitPack build

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,26 +1,22 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
     `maven-publish`
 }
 
-group = "com.github.ashuchoudhury"
-version = "1.0.0"
+group = "com.github.ashu-choudhury"
+version = "1.0.1"
 
 dependencies {
-    // Ktor for networking
     implementation("io.ktor:ktor-client-core:3.0.0")
     implementation("io.ktor:ktor-client-okhttp:3.0.0")
     implementation("io.ktor:ktor-client-content-negotiation:3.0.0")
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.0")
 
-    // Jsoup for HTML parsing
     implementation("org.jsoup:jsoup:1.18.1")
 
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
-    // Test dependencies
     testImplementation(kotlin("test"))
 }
 
@@ -32,9 +28,7 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
-            groupId = "com.github.ashuchoudhury"
             artifactId = "indian-rail-kotlin"
-            version = "1.0.0"
         }
     }
 }
